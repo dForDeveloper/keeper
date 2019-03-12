f382627d4b1879f2654673444bb2f36407a7b115
Jeo D <e8gea6w5z8id@opayq.com>
2019-03-12 11:34:20 -0600

    Updates

---
+++ precache-manifest.193cdd8d589eb114854f2c97d0b1dbb2.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ef43b89b40293284bd871e79ca6dd9b",
+    "url": "/static/media/edit.5ef43b89.svg"
+  },
+  {
+    "revision": "d60a898df7d44e27eb74",
+    "url": "/static/css/main.eb589d0e.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "befbdd2d29323948ef96",
+    "url": "/static/js/2.befbdd2d.chunk.js"
+  },
+  {
+    "revision": "284c00e23eaed23514a59e9275960940",
+    "url": "/static/media/uncheckedicon.284c00e2.svg"
+  },
+  {
+    "revision": "c959b72493e4309eec069897abb2bfbc",
+    "url": "/static/media/checkedicon.c959b724.svg"
+  },
+  {
+    "revision": "777b93c6d5a7b86081144f8d0d3ca1d8",
+    "url": "/static/media/deleteicon.777b93c6.svg"
+  },
+  {
+    "revision": "d60a898df7d44e27eb74",
+    "url": "/static/js/main.d60a898d.chunk.js"
+  },
+  {
+    "revision": "d98e9b1007e5681f76032d72eb189d4c",
+    "url": "/static/media/delete.d98e9b10.svg"
+  },
+  {
+    "revision": "0edd6a522f7f9ca7271f981049099d9c",
+    "url": "/static/media/newnoteicon.0edd6a52.svg"
+  },
+  {
+    "revision": "86fcd15e1834e206eaa4beaeae44c20c",
+    "url": "/static/media/googleicon.86fcd15e.svg"
+  },
+  {
+    "revision": "beacd3a51e17c6e3c97349c193ccf43c",
+    "url": "/static/media/paletteicon.beacd3a5.svg"
+  },
+  {
+    "revision": "eadd7bd9a38dbfc1435f1c67129eaa2d",
+    "url": "/static/media/notebook.eadd7bd9.svg"
+  },
+  {
+    "revision": "b03cc100ff1cd34a8eb10f16e4a3557a",
+    "url": "/static/media/blank-note-unsplash.b03cc100.jpg"
+  },
+  {
+    "revision": "e8fc1cbdee3b5fecad5a5780868c3e8a",
+    "url": "/index.html"
+  }
+];
